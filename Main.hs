01fc8a37efad4dcbf878a6906b0914d1a5a45a51
ben blum <bblum0@gmail.com>
2019-08-03 21:40:03 -0400

    toposort

--- Main.hs
+++ Main.hs
@@ -20,6 +20,25 @@ import Debug.Trace
 type Kanji = String
 type Recipe = String
 
+toposort :: Eq a => Map a [a] -> [[a]]
+toposort m = reverse $ snd $ until (null . fst) freeNodes (m, [])
+    where freeNodes :: Eq a => (Map a [a], [[a]]) -> (Map a [a], [[a]])
+          freeNodes (m, result) = (dependent, free:result)
+              where (free_m, dependent_m) = M.partition null m
+                    free = M.keys free_m
+                    dependent = M.map (\\ free) dependent_m
+
+testToposort :: [String] -- list of test failures
+testToposort = mapMaybe check tests
+    where check (x,y) = if toposort (M.fromList x) == y then Nothing else Just $ show x
+          free i = (i, []) -- for legibility
+          tests = [([], []),
+                   ([(0, [1,2]), free 1, free 2], [[1,2],[0]]),
+                   (
+                       [free 1, free 2, free 3, (4, [2]), (5, [1,3]), (6, [1,4])],
+                        [[1,2,3], [4,5], [6]]
+                   )]
+
 parseChise :: [String] -> Map Kanji Recipe
 parseChise chiseLines =
     let
@@ -31,6 +50,7 @@ parseChise chiseLines =
 
 main :: IO ()
 main = do
+    when (not $ null testToposort) $ error $ "test(s) failed: " ++ show testToposort
     chise <- readFile "chise.txt"
     (frequencyList :: [Kanji]) <- lines <$> readFile "frequent-joyo.txt"
     print (length frequencyList)
