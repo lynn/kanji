912d1940b370bb0c2aa169c8920cf9408d9e9aa7
Lynn <lynn@foldr.moe>
2019-08-04 02:55:12 +0200

    parse chise.txt

---
+++ Main.hs
@@ -0,0 +1,31 @@
+-- cabal install split
+module Main where
+
+import qualified Data.Map as M
+import Data.Map (Map)
+import qualified Data.Set as S
+import Data.Set (Set)
+import Data.List
+import Data.List.Split
+import Data.Maybe
+import Control.Applicative
+import Control.Monad
+import Debug.Trace
+
+type Kanji = String
+type Recipe = String
+
+parseChise :: [String] -> Map Kanji Recipe
+parseChise chiseLines =
+    let
+        isComment line = null line || take 1 line == ";"
+        recipeLines = filter (not . isComment) chiseLines
+        pairs = [(k, r) | line <- recipeLines, let (_:k:r:_) = splitOn "\t" line]
+    in
+        M.fromList pairs
+
+main :: IO ()
+main = do
+    chise <- readFile "chise.txt"
+    let recipes = parseChise (lines chise)
+    print (M.size recipes)
